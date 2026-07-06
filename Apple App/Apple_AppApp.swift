e6461305472c117ffd828bf7884b5bb59d19a00b
hughdwill-byte <hughdwill@gmail.com>
2026-07-06 10:39:45 +1000

    Initial Commit

---
+++ Apple App/Apple_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_AppApp.swift
+//  Apple App
+//
+//  Created by Hugh Williams on 6/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct Apple_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple App
+//
+//  Created by Hugh Williams on 6/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
